e0a8b4b0ef285f011df91b1aa118d9a17b936cff
tfermoso <tomas.fermoso@gmail.com>
2026-01-09 17:38:47 +0100

    alineando navbar

--- src/Views/tarea/index.php
+++ src/Views/tarea/index.php
@@ -31,46 +31,42 @@
 <body class="bg-light">
 
     <!-- NAVBAR -->
-   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
 
-        <a class="navbar-brand fw-bold" href="/">ProjectGest</a>
+            <a class="navbar-brand fw-bold" href="/">ProjectGest</a>
 
-        <!-- BOTÓN HAMBURGUESA -->
-        <button class="navbar-toggler" type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#navbarTareas"
-                aria-controls="navbarTareas"
-                aria-expanded="false"
-                aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+            <!-- BOTÓN HAMBURGUESA -->
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTareas"
+                aria-controls="navbarTareas" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-        <!-- MENÚ COLAPSABLE -->
-        <div class="collapse navbar-collapse justify-content-end" id="navbarTareas">
-            <ul class="navbar-nav">
+            <!-- MENÚ COLAPSABLE -->
+            <div class="collapse navbar-collapse justify-content-end" id="navbarTareas">
+                <ul class="navbar-nav align-items-center">
 
-                <li class="nav-item">
-                    <a href="<?= BASE_URL ?>proyecto" class="nav-link">Proyectos</a>
-                </li>
+                    <li class="nav-item">
+                        <a href="<?= BASE_URL ?>proyecto" class="nav-link">Proyectos</a>
+                    </li>
 
-                <!-- Usuario logueado -->
-                <li class="nav-item">
-                    <a href="#" class="nav-link">
-                        <i class="fas fa-user"></i>
-                        <?= htmlspecialchars($_SESSION['user_name']) ?>
-                    </a>
-                </li>
+                    <!-- Usuario logueado -->
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="fas fa-user"></i>
+                            <?= htmlspecialchars($_SESSION['user_name']) ?>
+                        </a>
+                    </li>
 
-                <li class="nav-item">
-                    <a href="<?= BASE_URL ?>auth/logout" class="nav-link">Salir</a>
-                </li>
+                    <li class="nav-item">
+                        <a href="<?= BASE_URL ?>auth/logout" class="nav-link">Salir</a>
+                    </li>
 
-            </ul>
+                </ul>
+            </div>
+
         </div>
-
-    </div>
-</nav>
+    </nav>
 
 
     <!-- CONTENT -->
